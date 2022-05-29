9326b30f0208c3049a0425dcc70b956edf329f24
williansoncini <willian_soncini@hotmail.com>
2022-05-29 11:06:58 -0300

    Seção 5 - colocando a api para funcionar

---
+++ 5 - CRUD com SQL MODEL/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from core.configs import settings
+from api.v1.api import router
+
+app = FastAPI()
+app.include_router(router, prefix=settings.API_V1_STR)
+
+if __name__ == '__main__':
+  import uvicorn
+  
+  uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, log_level='info')
+
